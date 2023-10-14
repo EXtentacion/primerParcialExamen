d2cd0dc706a00848d7ad848b62a7511f4c7f64fa
EXtentacion <125932096+EXtentacion@users.noreply.github.com>
2023-10-13 23:34:32 -0600

    Controlodares

--- PRIMERPARCIAL/examen/routes/web.php
+++ PRIMERPARCIAL/examen/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\diarioController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,15 +21,20 @@ Route::get('/', function () {
 
 
 
-Route::view('/', 'welcome')->name('apodoInicio');
-Route::view('/vista1', 'vista1')->name('apodoVista1');
+// Route::view('/', 'welcome')->name('apodoInicio');
+// Route::view('/vista1', 'vista1')->name('apodoVista1');
 
-Route::get('/vista2', function () {
-    return view('vista2'); 
-})->name('apodoVista2');
+// Route::get('/vista2', function () {
+//     return view('vista2'); 
+// })->name('apodoVista2');
 
 
-Route::get('/vista3', function () {
-    return view('vista3'); 
-})->name('apodoVista3');
+// Route::get('/vista3', function () {
+//     return view('vista3'); 
+// })->name('apodoVista3');
 
+
+
+Route::get('/', [diarioController::class, 'metodoInicio'])->name('apodo1');
+Route::get('/formulario', [diarioController::class, 'metodoFormulario'])->name('apodoFormulario');
+Route::get('/recuerdos', [diarioController::class, 'metodoRecuerdo'])->name('apodoRecuerdo');
